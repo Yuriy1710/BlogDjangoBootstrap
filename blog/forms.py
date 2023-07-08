ea6ce617639ef5379295cebdb8614b25cba51cb4
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2023-07-08 22:05:31 +0200

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from .models import Post
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        
+        widgets = {
+          'title': forms.TextInput(attrs={'class': 'form-control'}),
+          'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+          'author': forms.Select(attrs={'class': 'form-control'}),
+          'category': forms.Select(attrs={'class': 'form-control'}),
+          'body': SummernoteWidget(),
+        }
+        
+        
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'title_tag', 'body']
+        
+        widgets = {
+          'title': forms.TextInput(attrs={'class': 'form-control'}),
+          'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+          'body': SummernoteWidget(),
+        }
+        
